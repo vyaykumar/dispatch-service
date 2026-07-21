d785001e52f11602b530347fa93ecbd6d62172f3
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-07-21 21:49:02 +0530

    Second Pass at FieldWriter. Probably last one.

--- src/by_my_hand.cpp
+++ src/by_my_hand.cpp
@@ -7,13 +7,49 @@ enum class FieldId : uint8_t {
     kPayload = 4,
 };
 
-
-class message {
+class FieldWriter {
 public:
-    // Constructor, given type and size,
-    // that will initialize a uint8_t array, of size `length`.
+    void addString(FieldId id, const std::string_view value) {
+        payload_.emplace_back(static_cast<uint8_t>(id));
 
+        const uint32_t length = value.size();
+        for (size_t idx {0}; idx < 4; idx++)
+            payload_.emplace_back(length >> (8*(3-idx)) & 0xFF);
 
+        for (const auto& element : value)
+            payload_.emplace_back(static_cast<uint8_t>(element));
+    }
+    void addByte(FieldId id, uint8_t value) {
+        payload_.emplace_back(static_cast<uint8_t>(id));
+
+        constexpr uint32_t length = 1;
+        for (size_t idx {0}; idx < 4; idx++)
+            payload_.emplace_back(length >> (8*(3-idx)) & 0xFF);
+
+        payload_.emplace_back(value);
+    }
+    void addRaw(FieldId id, std::span<const uint8_t> value) {
+        payload_.emplace_back(static_cast<uint8_t>(id));
+
+        const uint32_t length = value.size();
+        for (size_t idx {0}; idx < 4; idx++)
+            payload_.emplace_back(length >> (8*(3-idx)) & 0xFF);
+
+        payload_.insert(payload_.end(), value.begin(), value.end());
+    }
+
+    // Inspection copy.
+    [[nodiscard]] const std::vector<uint8_t>& payload() const {
+        return payload_;
+    }
+
+    // Consumption copy. Do not use FieldWriter after usage.
+    [[nodiscard]] std::vector<uint8_t> finish() && {
+        return std::move(payload_);
+    }
+
+private:
+    std::vector<uint8_t> payload_;
 };
 
 // The following takes only one message inside its payload. What if we want to sent multiple at once?
